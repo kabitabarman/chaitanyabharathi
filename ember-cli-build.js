4a994adcdb790782707c3e1adb85c4741e15189c
kabitabarman <kabita.barman.it@gmail.com>
2017-04-28 18:29:00 -0700

    Website theme updated based on feedback

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    'ember-cli-bootswatch': 'simplex'
+    'ember-cli-bootswatch': 'readable'
   });
 
   // Use `app.import` to add additional libraries to the generated
